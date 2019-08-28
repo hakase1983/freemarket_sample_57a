95645c5f823ba8e796145bee82f6b35a75bc295d
ryo-fukumoto <gkrpw6767@gmail.com>
2019-08-28 13:57:20 +0900

    login_view_design
    ログイン画面のビュー作成

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :comments
+  has_many :likes
+  has_many :sellers
+  has_many :buyers
+  has_many :bought_items, foreign_key: "buyer_id"
+  has_many :selling_items, -> {where("buyer_id is NULL") }, foreign_key: "seller_id"
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id"
+  has_one :personal_info
+end
