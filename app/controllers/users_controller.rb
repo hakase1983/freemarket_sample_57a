f6645f48ebd6d2e8db657937edf01f32a4a92860
Iwamoto-Kenji <08030531kk>
2019-08-29 19:44:48 +0900

    ユーザー本人確認ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,5 @@
 class UsersController < ApplicationController
-  def index
-  end
-
   def edit
     @personal_info = PersonalInfo.new
-  end
-
-  def update
-    @personal_info = PersonalInfo.new
-    @personal_info.update
   end
 end
