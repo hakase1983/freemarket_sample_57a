4f3a50f168f443b5616bb823cf955b1e81df14ab
Iwamoto-Kenji <08030531kk>
2019-08-29 19:26:47 +0900

    ユーザー本人確認ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def edit
+    @personal_info = PersonalInfo.new
+  end
+
+  def update
+    @personal_info = PersonalInfo.new
+    @personal_info.update
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "items#index"
-  resources :items
+  # root to: "items#index"
+  # resources :items
+  root 'users#edit'
+  resources :users, only:[:update]
+  # root 'users#index'
+  get "users", to:"users#index"
 end
