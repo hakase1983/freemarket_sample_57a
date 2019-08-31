298c0c101bac4b502d00103c56bdadbf330c0faa
hakase1983 <son.of.liberty1983@gmail.com>
2019-08-31 18:21:54 +0900

    fix user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   has_many :items
   has_many :comments
   has_many :likes
