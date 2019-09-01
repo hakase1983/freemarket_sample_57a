9f3b7e3a2dae0365ff872007c6030cb3c924e3fc
Iwamoto-Kenji <08030531kk>
2019-09-01 13:27:47 +0900

    購入内容確認ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def confirmation
+    @personal_info = PersonalInfo.new
+    render layout: 'compact'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: "items#index"
+  # resources :items
   root 'users#edit'
-  resources :users, only:[:update]
+  get "items", to:"items#confirmation"
 end
