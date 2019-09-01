fb7172df26ca655418506d4bf4951197adf4c839
Iwamoto-Kenji <08030531kk>
2019-09-01 13:59:34 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "items#index"
-  # resources :items
-  root 'users#edit'
-  get "items", to:"items#confirmation"
+  root 'items#index'
+  resources :users
+  resources :items do
+    collection do
+      get "confirmation" 
+    end
+  end
 end
