7a28442f7d56e4edf6807fb8538ff008c0777d10
ryo-fukumoto <gkrpw6767@gmail.com>
2019-09-02 12:32:42 +0900

    delete_controller
    不要なコントローラーの削除
    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :signup do
     collection do
       get 'step1'
       get 'step2'
       get 'step3'
       get 'step4' 
-      get 'done' 
+      post 'done' 
     end
   end 
 end
