bb5ebce22bfe0cccaf627c66d4c08d2b5351c989
hakase1983 <son.of.liberty1983@gmail.com>
2019-09-02 18:10:43 +0900

    migation-file-edit

---
+++ db/migrate/20190827064036_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name ,null: false
+      t.text :description ,null: false
+      t.references :category ,null:false ,foreign_key: true
+      t.string :condition ,null: false
+      t.integer :price ,null: false
+      t.references :user ,null:false ,foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/migrate/20190828041656_create_personal_infos.rb
+++ db/migrate/20190828041656_create_personal_infos.rb
@@ -5,14 +5,14 @@ class CreatePersonalInfos < ActiveRecord::Migration[5.2]
       t.string :first_name,          null: false
       t.string :family_name_kana,    null: false
       t.string :first_name_kana,     null: false
-      t.string :postal_code,         null: false
+      t.integer :postal_code,        null: false
       t.string :prefecture_code,     null: false
       t.string :city_code,           null: false
       t.string :address_num,         null: false
-      t.date :birth_day,             null: false
-      t.references :user,            null: false, foreign_key: true
       t.string :building_name
-      t.integer :telephone
+      t.string :telephone
+      t.date :birth_day           
+      t.references :user,            null: false, foreign_key: true
       t.timestamps
     end
   end
