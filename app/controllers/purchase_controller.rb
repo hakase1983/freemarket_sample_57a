318766c0be6e2f197be36909a3acaf40d3c0ac57
ryo-fukumoto <gkrpw6767@gmail.com>
2019-09-10 15:10:50 +0900

    ハッシュロケット、コメントアウトの削除
    不要な記述の削除

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -5,15 +5,12 @@ class PurchaseController < ApplicationController
     @personal_info = PersonalInfo.find(current_user.id)
     @item = Item.find(params[:item_id])
     card = Card.where(user_id: current_user.id).first
-    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    
     if card.blank?
-      #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
       Payjp.api_key = ENV["PAY_JP_SECRET"]
-      #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
-      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
     render layout: 'compact'
@@ -24,13 +21,10 @@ class PurchaseController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAY_JP_SECRET']
     Payjp::Charge.create(
-    :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
-    :customer => card.customer_id, #顧客ID
-    :currency => 'jpy', #日本円
+    amount: @item.price, 
+    customer: card.customer_id, 
+    currency: 'jpy', 
   )
-  redirect_to  root_path #完了画面に移動
-  end
-
-  def purchase_done
+  redirect_to  root_path 
   end
 end
