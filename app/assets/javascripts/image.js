9ff5f9b1dfadd659b0864104792ad272cd339088
ryuki96 <51799058+ryuki96@users.noreply.github.com>
2019-09-11 16:13:23 +0900

    fixed commit

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,49 +1,132 @@
 $(function(){
-  var topHtml = `<li class="sell-upload-item" id="a">
-                  <figure class="sell-upload-figure">`
-  var bottomHtml = `<div class="sell-upload-button">
-                      <a href="" class="sell-upload-edit">編集</a>
-                      <a class="image-delete">削除</a>
-                    </div>`;    
-  var dropbox = `<label class="sell__image-dropbox1" for="item_image1" style="display: block;" id="1" name="dropbox">
-                  <input class="sell__image-dropfile" type="file" style="display:none;" id="item_image2" name="item[image_attributes][image2]">
-                  <pre>ドラッグアンドドロップ
-                  またはクリックしてファイルをアップロード</pre>
-                </label>`   
-  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
-  $('form').on('change','input[type="file"]', function(e) {
-    $(".sell__image-dropbox0").hide()
-    $(".sell__image-dropbox1").hide()
-    var file = e.target.files[0],
-        reader = new FileReader(),
-        t = this;
+  var dropzone = $('.dropzone-area');
+  var dropzone2 = $('.dropzone-area2');
+  var dropzone_box = $('.dropzone-box');
+  var images = [];
+  var inputs  =[];
+  var input_area = $('.input_area');
+  var preview = $('#preview');
+  var preview2 = $('#preview2');
 
-    // 画像ファイル以外の場合は何もしない
-    if(file.type.indexOf("image") < 0){
-      return false;
+  $('form').on('change', 'input[type= "file"].upload-image',function(event) {
+    var file = $(this).prop('files')[0];
+    var reader = new FileReader();
+    inputs.push($(this));
+    var topHtml = $(`<li class="sell-upload-item" id="a"><figure class="sell-upload-figure">`)
+    topHtml.append(img);
+    var img = $(`<div class= "img_view"><img></div>`);
+    reader.onload = function(e) {
+      var btn_wrapper = $(`<div class="sell-upload-button"><div class="sell-upload-edit">編集</div><a class="image-delete">削除</a></div>`);
+      topHtml.append(btn_wrapper);
+      topHtml.find('img').attr({
+        src: e.target.result
+      })
     }
-
-    // ファイル読み込みが完了した際のイベント登録
-    reader.onload = (function(file) {
-      return function(e) {
-        //既存のプレビューを削除
-        var drop_id = $('label[name="dropbox"]').attr('id');
-        if ( drop_id == "0")
-          $(".sell-upload-items ul").append(topHtml)
-          // .prevewの領域の中にロードした画像を表示するimageタグを追加
-          $(".sell-upload-figure").append($('<img>').attr({
-                    src: e.target.result,
-                    width: "114px",
-                    height: "85px",
-                    class: "preview",
-                    id: "image1",
-                    title: file.name
-                }));
-          $(".sell-upload-items li").append(bottomHtml)
-          $(".sell__image-container").append(dropbox)
-          
-        };
-    })(file);
     reader.readAsDataURL(file);
+    images.push(topHtml);
+
+    if(images.length >= 5) {
+      dropzone2.css({
+        'display': 'inline-block'
+      })
+      dropzone.css({
+        'display': 'none'
+      })
+      $.each(images, function(index, image) {
+        image.attr('data-image', index);
+        preview2.append(image);
+        dropzone2.css({
+          'width': `calc(100% - (125px * ${images.length - 5}))`
+        })
+      })
+      if(images.length == 9) {
+        dropzone2.find('p').replaceWith('<i class="fa fa-camera"></i>')
+      }
+    } else {
+        $('#preview').empty();
+        $.each(images, function(index, image) {
+          image.attr('data-image', index);
+          preview.append(image);
+        })
+        dropzone.css({
+          'width': `calc(100% - (125px * ${images.length}))`
+        })
+        dropzone.css('display','inline-block')
+      }
+      if(images.length == 4) {
+        dropzone.find('p').replaceWith('<i class="fa fa-camera"></i>')
+      }
+    if(images.length == 10) {
+      dropzone2.css({
+        'display': 'none'
+      })
+      return;
+    }
+    var new_image = $(`<input multiple= "multiple" name="images[name][]" class="upload-image" data-image= ${images.length} type="file" id="upload-image">`);
+    input_area.prepend(new_image);
   });
+  $(document).on('click', '.image-delete', function() {
+    var target_image = $(this).parent().parent();
+    $.each(inputs, function(index, input){
+      if ($(this).data('image') == target_image.data('image')){
+        $(this).remove();
+        target_image.remove();
+        var num = $(this).data('image');
+        images.splice(num, 1);
+        inputs.splice(num, 1);
+        if(inputs.length == 0) {
+          $('input[type= "file"].upload-image').attr({
+            'data-image': 0
+          })
+        }
+      }
+    })
+    $('input[type= "file"].upload-image:first').attr({
+      'data-image': inputs.length
+    })
+    $.each(inputs, function(index, input) {
+      var input = $(this)
+      input.attr({
+        'data-image': index
+      })
+      $('input[type= "file"].upload-image:first').after(input)
+    })
+    if (images.length >= 5) {
+      dropzone2.css({
+        'display': 'block'
+      })
+      $.each(images, function(index, image) {
+        image.attr('data-image', index);
+        preview2.append(image);
+      })
+      dropzone2.css({
+        'width': `calc(100% - (125px * ${images.length - 5}))`
+      })
+      if(images.length == 9) {
+        dropzone2.find('p').replaceWith('<i class="fa fa-camera"></i>')
+      }
+      if(images.length == 8) {
+        dropzone2.find('i').replaceWith('<pre>ドラッグアンドドロップまたはクリックしてファイルをアップロード</pre>')
+      }
+    } else {
+      dropzone.css({
+        'display': 'block'
+      })
+      $.each(images, function(index, image) {
+        image.attr('data-image', index);
+        preview.append(image);
+      })
+      dropzone.css({
+        'width': `calc(100% - (125px * ${images.length}))`
+      })
+    }
+    if(images.length == 4) {
+      dropzone2.css({
+        'display': 'none'
+      })
+    }
+    if(images.length == 3) {
+      dropzone.find('i').replaceWith('<pre>ドラッグアンドドロップまたはクリックしてファイルをアップロード</pre>')
+    }
+  })
 });
