94c94b735445df62c17c7ecdeee5c32152d4d285
hakase1983 <son.of.liberty1983@gmail.com>
2019-09-12 21:49:05 +0900

    ファイル修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  layouts "compact"
+  layout "compact"
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
